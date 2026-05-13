519ce1d4f81fc23118cfba42d15532cbaa813440
Laura Leiton <lauraleiton@unimayor.edu.co>
2026-05-13 11:29:31 -0500

    configuracion para el servidor del json

--- src/app/shared/services/user.ts
+++ src/app/shared/services/user.ts
@@ -4,48 +4,47 @@ import { Observable, throwError } from 'rxjs';
 import { map, switchMap, catchError } from 'rxjs/operators';
 import { User } from '../models/user.model';
 
-
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  private apiUrl = 'http://localhost:3000/users';
+  private apiUrl =
+    'message": "You need to pass X-Master-Key or X-Access-Key in the header to read a private bin';
 
   constructor(private http: HttpClient) {}
 
- register(name: string, email: string, password: string): Observable<User> {
-  const ts = Date.now();
-  return this.http.get<User[]>(`${this.apiUrl}?email=${email}&_=${ts}`).pipe(
-    switchMap(users => {
-      console.log('GET users response:', users);
-      const list = users ?? [];
-      if (list.length > 0) {
-        console.log('Correo ya existe');
-        return throwError(() => new Error('Este correo ya está registrado'));
-      }
-      const newUser: User = {
-        id: Date.now(),
-        name,
-        email,
-        password,
-        createdAt: new Date(),
-      };
-      console.log('POST new user:', newUser);
-      return this.http.post<User>(this.apiUrl, newUser);
-    }),
-    catchError(err => {
-      console.error('Error en register()', err);
-      const msg = err?.error?.message || err?.message || 'Error en el servidor';
-      return throwError(() => new Error(msg));
-    })
-  );
-}
-
+  register(name: string, email: string, password: string): Observable<User> {
+    const ts = Date.now();
+    return this.http.get<User[]>(`${this.apiUrl}?email=${email}&_=${ts}`).pipe(
+      switchMap((users) => {
+        console.log('GET users response:', users);
+        const list = users ?? [];
+        if (list.length > 0) {
+          console.log('Correo ya existe');
+          return throwError(() => new Error('Este correo ya está registrado'));
+        }
+        const newUser: User = {
+          id: Date.now(),
+          name,
+          email,
+          password,
+          createdAt: new Date(),
+        };
+        console.log('POST new user:', newUser);
+        return this.http.post<User>(this.apiUrl, newUser);
+      }),
+      catchError((err) => {
+        console.error('Error en register()', err);
+        const msg = err?.error?.message || err?.message || 'Error en el servidor';
+        return throwError(() => new Error(msg));
+      }),
+    );
+  }
 
   authenticateUser(email: string, password: string): Observable<User | null> {
     return this.http
       .get<User[]>(`${this.apiUrl}?email=${email}&password=${password}`)
-      .pipe(map(users => (users.length > 0 ? users[0] : null)));
+      .pipe(map((users) => (users.length > 0 ? users[0] : null)));
   }
 
   getAllUsers(): Observable<User[]> {
