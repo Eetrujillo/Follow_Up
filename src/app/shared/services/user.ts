d90d6c61f040a1a502b10881ba775e8337f3b96b
Esteban Trujillo <etrujillo@unimayor.edu.co>
2026-05-13 23:16:16 -0500

    Fix API URL to include meta parameter in UserService

--- src/app/shared/services/user.ts
+++ src/app/shared/services/user.ts
@@ -8,7 +8,7 @@ import { User } from '../models/user.model';
   providedIn: 'root',
 })
 export class UserService {
-  private apiUrl = 'https://api.jsonbin.io/v3/b/6a04ae5badc21f119a95e55d';
+  private apiUrl = 'https://api.jsonbin.io/v3/b/6a04ae5badc21f119a95e55d?meta=false';
 
   constructor(private http: HttpClient) {}
 
